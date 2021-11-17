697c55537d8343c1592d4bd08823b15eb9efbb82
Ismael <isma907@gmail.com>
2021-11-17 15:19:08 -0300

    added interfaces, ajax call and list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CongressListComponent} from './congress-list/congress-list.component';
 import {UiModuleModule} from "./ui-module/ui-module.module";
+import {MatTableModule} from "@angular/material/table";
+import {HttpClientModule} from "@angular/common/http";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import {UiModuleModule} from "./ui-module/ui-module.module";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    UiModuleModule
+    HttpClientModule,
+    UiModuleModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/congress-list/congress-list.component.ts
+++ src/app/congress-list/congress-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CongressService} from "../shared/services/congress.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {ICongressMember} from "../shared/interfaces/congress.interface";
+import {finalize, take} from "rxjs";
 
 @Component({
   selector: 'lenio-congress-list',
   templateUrl: './congress-list.component.html',
   styleUrls: ['./congress-list.component.scss']
 })
+
 export class CongressListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private congressService: CongressService
+  ) {
   }
 
+  displayedColumns: string[] = ['first_name', 'party', 'gender', 'actions'];
+  dataSource = new MatTableDataSource<ICongressMember>([]);
+
+  ngOnInit() {
+    this.fetchMembers();
+  }
+
+  fetchMembers() {
+    this.congressService.getCongressList()
+      .pipe(
+        take(1),
+        finalize(() => {
+        })
+      )
+      .subscribe(
+        (members: ICongressMember[]) => {
+          this.dataSource.data = members;
+          this.dataSource.sort
+        },
+        (err) => {
+        },
+      );
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
